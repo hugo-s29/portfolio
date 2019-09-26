bd551c62ea3c0f1dd38653971c9c38e6614d5f07
Hugo_s29 <y.hugo.s29@gmail.com>
2019-09-26 17:38:07 +0200

    Modif of the navigation

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,69 +2,71 @@ import * as React from 'react'
 import Link from 'next/link'
 import styled from 'styled-components'
 
-const Nav = styled.nav`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  font-family: 'Raleway', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  color: #ffc54f;
-  min-height: 40px;
-
-  h3 {
-    font-weight: bold;
-  }
-
-  ul {
+const NavComponent: React.SFC<{}> = () => {
+  const Nav = styled.nav`
     display: flex;
     justify-content: space-around;
     align-items: center;
-    width: 40%;
+    font-family: 'Raleway', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    color: #ffc54f;
+    min-height: 40px;
 
-    li {
-      list-style: none;
+    h3 {
+      font-weight: bold;
+    }
 
-      a {
-        color: #ffc54f;
-        font-weight: 300;
-        background: linear-gradient(to top, #ffc54f 0%, #ffc54f 6%, transparent 6.01%) no-repeat left bottom / 0 100%;
-        text-decoration: none;
-        transition: background-size 0.5s;
+    ul {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      width: 40%;
 
-        &:hover {
-          background-size: 100% 100%;
+      li {
+        list-style: none;
+
+        a {
+          color: #ffc54f;
+          font-weight: 300;
+          background: linear-gradient(to top, #ffc54f 0%, #ffc54f 6%, transparent 6.01%) no-repeat left bottom / 0 100%;
+          text-decoration: none;
+          transition: background-size 0.5s;
+
+          &:hover {
+            background-size: 100% 100%;
+          }
         }
       }
+      @media screen and (max-width: 1280px) {
+        width: 60%;
+      }
     }
-    @media screen and (max-width: 1280px) {
-      width: 60%;
+    @media screen and (max-width: 600px) {
+      flex-direction: column;
     }
-  }
-  @media screen and (max-width: 600px) {
-    flex-direction: column;
-  }
-`
+  `
 
-const NavComponent: React.SFC<{}> = () => (
-  <Nav>
-    <h3>Hugos29</h3>
-    <ul>
-      <li>
-        <Link href="/yearly">
-          <a>Annuel</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/tools">
-          <a>Outils</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/blog">
-          <a>Blog</a>
-        </Link>
-      </li>
-    </ul>
-  </Nav>
-)
+  return (
+    <Nav>
+      <h3>Hugos29</h3>
+      <ul>
+        <li>
+          <Link href="/yearly">
+            <a>Annuel</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/tools">
+            <a>Outils</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
+        </li>
+      </ul>
+    </Nav>
+  )
+}
 
 export default NavComponent
